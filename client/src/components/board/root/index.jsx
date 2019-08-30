b4dd96d13b1bea4034743ee1966a061e5345b63e
John Trocchi <jctbog9@aol.com>
2019-08-30 13:10:23 -0400

    add move piece and select piece function to board component

--- client/src/components/board/root/index.jsx
+++ client/src/components/board/root/index.jsx
@@ -8,19 +8,23 @@ class Board extends Component {
   constructor(){
     super()
     this.state = {
-      initialBoard: []
+      initialBoard: [],
+      selectedPiece: undefined,
+      hasMountedFirst: false
     }
+    this.selectPiece = this.selectPiece.bind(this)
+    this.movePiece = this.movePiece.bind(this)
   }
 
   componentDidMount(){
-    this.setState({ initialBoard: this.createBoard()})
+    this.setState({ hasMountedFirst: true, initialBoard: this.createBoard()})
   }
 
   createBoard() {
     let rows = []
-    for(let i = 0, rowNumber = 1; i < 8; i++, rowNumber += 1){
+    for(let i = 0, rowNumber = 0; i < 8; i++, rowNumber += 1){
       const row = this.createRow(i, rowNumber)
-      rows.push(row)
+      rows = rows.concat(row)
     }
     return rows
   }
@@ -33,6 +37,24 @@ class Board extends Component {
     }
   }
 
+  selectPiece(event) {
+    const image = event.target.src
+    this.setState({ selectedPiece: image })
+    let newBoard = this.state.initialBoard
+    newBoard[event.target.id] = <BoardSquare {...newBoard[event.target.id].props} piece={undefined} key={newBoard[event.target.id].props.id}/>
+    this.setState({ initialBoard: newBoard })
+  }
+
+  movePiece(event) {
+    if(this.state.selectedPiece){
+      let newBoard = this.state.initialBoard
+      newBoard[event.target.id] = <BoardSquare {...newBoard[event.target.id].props} piece={this.state.selectedPiece} key={newBoard[event.target.id].props.id}/>
+      this.setState({ initialBoard: newBoard, selectedPiece: undefined })
+    } else {
+      this.selectPiece(event)
+    }
+  }
+
   createEvenRow(rowNumber){
     let row = []
     for(let i = 0; i < 8; i++){
@@ -40,30 +62,36 @@ class Board extends Component {
         row.push(
           <BoardSquare 
             styles={styles.blackSquare} 
-            key={i} 
+            id={rowNumber * 8 + i} 
+            key={rowNumber * 8 + i} 
             coordinates={{
               x: xAxis[i],
               y: rowNumber
             }}
-            initialPiece={this.determinePiece({
+            piece={this.determinePiece({
               x: xAxis[i],
               y: rowNumber
             })}
+            movePiece={this.movePiece}
+            selectPiece={this.selectPiece}
           />
         )
       ) : (
         row.push(
           <BoardSquare 
             styles={styles.whiteSquare} 
-            key={i} 
+            id={rowNumber * 8 + i} 
+            key={rowNumber * 8 + i} 
             coordinates={{
               x: xAxis[i],
               y: rowNumber
             }}
-            initialPiece={this.determinePiece({
+            piece={this.determinePiece({
               x: xAxis[i],
               y: rowNumber
             })}
+            movePiece={this.movePiece}
+            selectPiece={this.selectPiece}
           />
         )
       )
@@ -78,30 +106,36 @@ class Board extends Component {
         row.push(
           <BoardSquare 
             styles={styles.whiteSquare} 
-            key={i} 
+            id={rowNumber * 8 + i}
+            key={rowNumber * 8 + i} 
             coordinates={{
               x: xAxis[i],
               y: rowNumber
             }}
-            initialPiece={this.determinePiece({
+            piece={this.determinePiece({
               x: xAxis[i],
               y: rowNumber
             })}
+            movePiece={this.movePiece}
+            selectPiece={this.selectPiece}
           />
         )
       ) : (
         row.push(
           <BoardSquare 
             styles={styles.blackSquare} 
-            key={i} 
+            id={rowNumber * 8 + i}
+            key={rowNumber * 8 + i} 
             coordinates={{
               x: xAxis[i],
               y: rowNumber
             }}
-            initialPiece={this.determinePiece({
+            piece={this.determinePiece({
               x: xAxis[i],
               y: rowNumber
             })}
+            movePiece={this.movePiece}
+            selectPiece={this.selectPiece}
           />
         )
       )
@@ -110,43 +144,43 @@ class Board extends Component {
   }
   
   determinePiece(coordinates){
-    if(coordinates.y === 1){
-      if(coordinates.x === 'a' || coordinates.x === 'h'){
+    if(coordinates.y === 0){
+      if(coordinates.x === 0 || coordinates.x === 7){
         return black.rook
       }
-      if(coordinates.x === 'b' || coordinates.x === 'g'){
+      if(coordinates.x === 1 || coordinates.x === 6){
         return black.knight
       }
-      if(coordinates.x === 'c' || coordinates.x === 'f'){
+      if(coordinates.x === 2 || coordinates.x === 5){
         return black.bishop
       }
-      if(coordinates.x === 'd'){
+      if(coordinates.x === 3){
         return black.queen
       }
-      if(coordinates.x === 'e'){
+      if(coordinates.x === 4){
         return black.king
       }
     }
-    if(coordinates.y === 2){
+    if(coordinates.y === 1){
       return black.pawn
     }
-    if(coordinates.y === 7){
+    if(coordinates.y === 6){
       return white.pawn
     }
-    if(coordinates.y === 8){
-      if(coordinates.x === 'a' || coordinates.x === 'h'){
+    if(coordinates.y === 7){
+      if(coordinates.x === 0 || coordinates.x === 7){
         return white.rook
       }
-      if(coordinates.x === 'b' || coordinates.x === 'g'){
+      if(coordinates.x === 1 || coordinates.x === 6){
         return white.knight
       }
-      if(coordinates.x === 'c' || coordinates.x === 'f'){
+      if(coordinates.x === 2 || coordinates.x === 5){
         return white.bishop
       }
-      if(coordinates.x === 'd'){
+      if(coordinates.x === 3){
         return white.queen
       }
-      if(coordinates.x === 'e'){
+      if(coordinates.x === 4){
         return white.king
       }
     }
--- client/src/constants/grid.js
+++ client/src/constants/grid.js
@@ -1,2 +1,2 @@
-export const xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-export const yAxis = [1, 2, 3, 4, 5, 6, 7, 8]
+export const xAxis = [0, 1, 2, 3, 4, 5, 6, 7]
+export const yAxis = [0, 1, 2, 3, 4, 5, 6, 7]
